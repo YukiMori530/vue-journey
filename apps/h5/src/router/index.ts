1c23d9ae91d4209454724c6b53aa0227388c00aa
liuzixi <alexanderseabra123@gmail.com>
2026-06-10 10:18:56 +0800

    fix: simplify tsconfig to remove editor errors

--- apps/h5/src/router/index.ts
+++ apps/h5/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/home-view.vue'
-import PlanView from '@/views/plan-view.vue'
-import MineView from '@/views/mine-view.vue'
+import HomeView from '../views/home-view.vue'
+import PlanView from '../views/plan-view.vue'
+import MineView from '../views/mine-view.vue'
 
 const router = createRouter({
   history: createWebHistory(),
--- apps/h5/vite.config.ts
+++ apps/h5/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [
@@ -11,9 +10,4 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 })
